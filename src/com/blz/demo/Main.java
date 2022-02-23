20c2689ab04fa1ecb1c90811d88f3bb3d19088f6
karthikbenki <karthikmc007@gmail.com>
2022-02-23 10:52:00 +0530

    Refactored: to generic to find maximum

--- src/com/blz/demo/Main.java
+++ src/com/blz/demo/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to check max of three variables");
-        String  x = "jack",y="king",z="queen";
-        TestMaximum.testMaximum(x,y,z);
+        String x = "jack", y = "king", z = "queen";
+        Integer a = 25, b = 24, c = 19;
+        Float p = 1.23f, q = 2.2f, r = 0.2f;
+        TestMaximum.testMaximum(x, y, z);
+        TestMaximum.testMaximum(a, b, c);
+        TestMaximum.testMaximum(p, q, r);
     }
 }
